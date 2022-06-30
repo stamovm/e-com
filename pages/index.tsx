89a440ea50f50a0eef6d2b6113759a350b1a2e99
stamovm <m.stamov@yahoo.com>
2022-06-30 15:50:42 -0700

    Created Next.js app and added Tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>E-Commerce</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
+        <h1 className="text-3xl font-bold ">Hello word</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
